9964bbc94ce20fce559cbbe47a96260c1975619d
howyi <howyi.lq@gmail.com>
2019-06-14 14:11:06 +0900

    fix env

--- amplify/backend/function/getWorkspaceCache/src/index.js
+++ amplify/backend/function/getWorkspaceCache/src/index.js
@@ -5,9 +5,34 @@ var region = process.env.REGION
 
 Amplify Params - DO NOT EDIT */
 
+const AWS = require('aws-sdk');
+var apiFutonGraphQLAPIIdOutput = process.env.API_FUTON_GRAPHQLAPIIDOUTPUT;
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+let workspaceTableName = 'Workspace-' + apiFutonGraphQLAPIIdOutput;
+if(process.env.ENV && process.env.ENV !== "NONE") {
+  workspaceTableName = workspaceTableName + '-' + process.env.ENV;
+}
+
 exports.handler = function (event, context) { //eslint-disable-line
   console.log(event.arguments);
   let workspaceId = event.arguments.workspaceId;
 
-  context.done(null, event.arguments.workspaceId);
+  var params = {
+    TableName: workspaceTableName,
+    Key:{
+      id: workspaceId
+    }
+  };
+  console.log(params);
+
+  dynamodb.get(params, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+    context.done(null, event.arguments.workspaceId);
+  });
 };
